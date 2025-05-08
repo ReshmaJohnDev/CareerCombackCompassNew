dcefaf52b237c39caae015845fe8f09673b4fb3e
Reshma John <reshma.chackola@gmail.com>
2025-05-08 10:24:33 +0200

    folder restructuring

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,10 @@
-# app/main.py
+import sys
+import os
 from fastapi import FastAPI
 from JobTracker.routes.job_routes import router as job_router
 
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 app = FastAPI()
 
 # Register routes
