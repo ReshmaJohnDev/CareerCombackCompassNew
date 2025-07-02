d6aeef8b12cd4047019a785063c40ea5bdc7e109
Reshma John <reshma.chackola@gmail.com>
2025-07-02 19:22:42 +0200

    Render URL added

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://your-backend-url.onrender.com",
   withCredentials: true, // optional: only if using cookies
 });
 
